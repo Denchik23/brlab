d75a1710de4262f6e5bb22e746d05d51359251b1
Denis Kochanov <kochanov.denis5@yandex.ru>
2019-10-08 08:56:25 +0300

    End bacend

--- wp-content/plugins/bets/bets-functions.php
+++ wp-content/plugins/bets/bets-functions.php
@@ -24,7 +24,9 @@ function best_register_post_type_init() {
 		'has_archive' => true, 
 		'menu_icon' => 'dashicons-image-filter', // иконка в меню
 		'menu_position' => 20, // порядок в меню
-		'supports' => array( 'title', 'editor', 'comments', 'author'),
+		'supports' => array( 'title', 'editor', 'comments', 'author', 'custom-fields'),
+                'capability_type' => 'best',
+                'map_meta_cap' => true,
 		'taxonomies' => array('bidtype')
 	);
 	register_post_type('best', $args);
@@ -35,41 +37,42 @@ function best_register_post_type_init() {
 add_action( 'init', 'create_taxonomy_bidtype' );
 function create_taxonomy_bidtype() {
 
-	// список параметров: wp-kama.ru/function/get_taxonomy_labels
-	register_taxonomy( 'bidtype', [ 'best' ], [ 
-		'label'                 => '', // определяется параметром $labels->name
-		'labels'                => [
-			'name'              => 'Тип ставки',
-			'singular_name'     => 'Genre',
-			'search_items'      => 'Search ставки',
-			'all_items'         => 'All ставки',
-			'view_item '        => 'View ставки',
-			'parent_item'       => 'Parent ставки',
-			'parent_item_colon' => 'Parent ставки:',
-			'edit_item'         => 'Edit ставки',
-			'update_item'       => 'Update ставки',
-			'add_new_item'      => 'Add New ставки',
-			'new_item_name'     => 'New Genre Name',
-			'menu_name'         => 'Тип ставки',
-		],
-		'description'           => '', // описание таксономии
-		'public'                => true,
-		// 'publicly_queryable'    => null, // равен аргументу public
-		// 'show_in_nav_menus'     => true, // равен аргументу public
-		// 'show_ui'               => true, // равен аргументу public
-		// 'show_in_menu'          => true, // равен аргументу show_ui
-		// 'show_tagcloud'         => true, // равен аргументу show_ui
-		// 'show_in_quick_edit'    => null, // равен аргументу show_ui
-		'hierarchical'          => false,
+    // список параметров:
+    register_taxonomy( 'bidtype', [ 'best' ], [ 
+        'label'                 => '', // определяется параметром $labels->name
+        'labels'                => [
+            'name'              => 'Тип ставки',
+            'singular_name'     => 'Genre',
+            'search_items'      => 'Search ставки',
+            'all_items'         => 'All ставки',
+            'view_item '        => 'View ставки',
+            'parent_item'       => 'Parent ставки',
+            'parent_item_colon' => 'Parent ставки:',
+            'edit_item'         => 'Edit ставки',
+            'update_item'       => 'Update ставки',
+            'add_new_item'      => 'Add New ставки',
+            'new_item_name'     => 'New Genre Name',
+            'menu_name'         => 'Тип ставки',
+        ],
+        'description'           => '', // описание таксономии
+        'public'                => true,
+        // 'publicly_queryable'    => null, // равен аргументу public
+        // 'show_in_nav_menus'     => true, // равен аргументу public
+        // 'show_ui'               => true, // равен аргументу public
+        // 'show_in_menu'          => true, // равен аргументу show_ui
+        // 'show_tagcloud'         => true, // равен аргументу show_ui
+        // 'show_in_quick_edit'    => null, // равен аргументу show_ui
+        'hierarchical'          => false,
 
-		'rewrite'               => true,
-		//'query_var'             => $taxonomy, // название параметра запроса
-		'capabilities'          => array(),
-		'meta_box_cb'           => null, // html метабокса. callback: `post_categories_meta_box` или `post_tags_meta_box`. false — метабокс отключен.
-		'show_admin_column'     => false, // авто-создание колонки таксы в таблице ассоциированного типа записи. (с версии 3.5)
-		'show_in_rest'          => null, // добавить в REST API
-		'rest_base'             => null, // $taxonomy
-		// '_builtin'              => false,
-		//'update_count_callback' => '_update_post_term_count',
-	] );
+        'rewrite'               => true,
+        //'query_var'             => $taxonomy, // название параметра запроса
+        'capabilities'          => array('assign_terms' => 'edit_bests'),
+        'meta_box_cb'           => null, // html метабокса. callback: `post_categories_meta_box` или `post_tags_meta_box`. false — метабокс отключен.
+        'show_admin_column'     => false, // авто-создание колонки таксы в таблице ассоциированного типа записи. (с версии 3.5)
+        'show_in_rest'          => null, // добавить в REST API
+        //'rest_base'             => null, // $taxonomy
+        // '_builtin'              => false,
+        //'update_count_callback' => '_update_post_term_count',
+    ] );
 }
+
--- wp-content/plugins/bets/bets.php
+++ wp-content/plugins/bets/bets.php
@@ -9,17 +9,106 @@ define( 'BETS__PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 //JBdump(BETS__PLUGIN_DIR);
 require_once( BETS__PLUGIN_DIR . 'bets-functions.php' );
 
+//права для капперов
+function bets_add_caps(){
+    $admin = get_role( 'administrator' );
+    $admin->add_cap( 'read_best' );
+    $admin->add_cap( 'read_private_best' );
+    $admin->add_cap( 'edit_best' );
+    $admin->add_cap( 'edit_bests' );
+    $admin->add_cap( 'edit_others_bests' );
+    $admin->add_cap( 'edit_published_bests' );
+    $admin->add_cap( 'edit_private_bests' );
+    $admin->add_cap( 'delete_bests' );
+    $admin->add_cap( 'delete_best' );
+    $admin->add_cap( 'delete_others_bests' );
+    $admin->add_cap( 'delete_published_best' );
+    $admin->add_cap( 'delete_best' );
+    $admin->add_cap( 'delete_private_best' );
+    $admin->add_cap( 'publish_bests' );
+    $admin->add_cap( 'moderate_best_comments' );
+    
+    $moderator = get_role( 'moderator' );
+    $moderator->add_cap( 'read_best' );
+    $moderator->add_cap( 'read_private_best' );
+    $moderator->add_cap( 'edit_best' );
+    $moderator->add_cap( 'edit_bests' );
+    $moderator->add_cap( 'edit_others_bests' );
+    $moderator->add_cap( 'edit_published_bests' );
+    $moderator->add_cap( 'edit_private_bests' );
+    $moderator->add_cap( 'delete_best' );
+    $moderator->add_cap( 'delete_bests' );
+    $moderator->add_cap( 'delete_others_bests' );
+    $moderator->add_cap( 'delete_published_bests' );
+    $moderator->add_cap( 'delete_private_best' );
+    $moderator->add_cap( 'publish_bests' );
+    $moderator->add_cap( 'moderate_best_comments' );
+    
+    
+    $capper = get_role( 'capper' );
+    $capper->add_cap( 'read_best' );
+    $capper->add_cap( 'read_private_best' );
+    $capper->add_cap( 'edit_best' );
+    $capper->add_cap( 'edit_bests' );
+    $capper->add_cap( 'edit_published_bests' );
+    $capper->add_cap( 'edit_private_bests' );
+    $capper->add_cap( 'delete_best' );
+    $capper->add_cap( 'delete_bests' );
+    $capper->add_cap( 'publish_bests' );
+}
+add_action( 'admin_init', 'bets_add_caps');
+
+
 //Сбрасывае урл
 register_activation_hook( __FILE__, 'best_install' ); 
 function best_install() {
-	// Запускаем функцию регистрации типа записи
-	best_register_post_type_init();
-	//добавляем роли
-	add_role('capper', 'Каппер', array(
-		'read' => true,
-		
-	));
-	// Сбрасываем настройки ЧПУ, чтобы они пересоздались с новыми данными
-	flush_rewrite_rules();
+    // Запускаем функцию регистрации типа записи
+    best_register_post_type_init();
+    //добавляем роли
+    add_role('capper', 'Каппер', array(
+        'read' => true,
+    ));
+    add_role('moderator', 'Модератор', array(
+        'read' => true,
+    ));
+    // Сбрасываем настройки ЧПУ, чтобы они пересоздались с новыми данными
+    flush_rewrite_rules();
 }
+
+//Очистка 
+function bets_remove_roles(){
+    //проверяем, существует ли роль, перед тем как ее удалить
+    if( get_role('capper') ){
+        remove_role( 'capper' );
+    }
+    if( get_role('moderator') ){
+        remove_role( 'moderator' );
+    }
+
+    $admin = get_role( 'administrator' );
+
+    $caps = array(
+        'read_best',
+        'read_private_best',
+        'edit_best',
+        'edit_bests',
+        'edit_others_bests',
+        'edit_published_bests',
+        'edit_private_bests',
+        'delete_bests',
+        'delete_best',
+        'delete_others_bests',
+        'delete_published_best',
+        'delete_best',
+        'delete_private_best',
+        'publish_bests',
+        'moderate_best_comments'
+    );
+
+    foreach ( $caps as $cap ) {
+        $admin->remove_cap( $cap );
+    }	
+}
+register_deactivation_hook( __FILE__, 'bets_remove_roles' );
+
 ?>
