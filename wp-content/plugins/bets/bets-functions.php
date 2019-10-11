7e7dd260aba10a1cb90ff9f79d10802b98e92230
Denis Kochanov <kochanov.denis5@yandex.ru>
2019-10-11 12:32:28 +0300

    End form add meta vate_bet

--- wp-content/plugins/bets/bets-functions.php
+++ wp-content/plugins/bets/bets-functions.php
@@ -76,3 +76,114 @@ function create_taxonomy_bidtype() {
     ] );
 }
 
+
+
+
+// Подключаем локализацию в самом конце подключаемых к выводу скриптов, чтобы скрипт
+// 'twentyfifteen-script', к которому мы подключаемся, точно был добавлен в очередь на вывод.
+add_action( 'wp_enqueue_scripts', 'btajax_data', 99 );
+function btajax_data(){
+    wp_localize_script( 'main.min', 'btajax', 
+        array(
+            'url' => admin_url('admin-ajax.php')
+        )
+    );  
+
+}
+
+if( wp_doing_ajax() ){
+    add_action( 'wp_ajax_best_action', 'best_action_callback' );
+    add_action('wp_ajax_nopriv_best_action', 'best_action_callback');
+}
+function best_action_callback() {
+    
+    $backArray = array();
+    $bet_vote = (isset($_POST['betvote'])? $_POST['betvote']: 0);
+    $bet_id =  (isset($_POST['betid'])? $_POST['betid']: 0);
+    
+    if (!is_numeric($bet_vote) || $bet_vote == 0) {
+        //error_log('Зашли в 1');
+        $backArray['success'] = false;
+        $backArray['html'] = '<div class="alert alert-danger" role="alert">Введено не верное значение!</div>';
+    } elseif (empty($bet_id) || !is_numeric($bet_id) || $bet_id == "") {
+        //error_log('Зашли в 2');
+        $backArray['success'] = false;
+        $backArray['html'] = '<div class="alert alert-danger" role="alert">Не верный id поста!</div>';
+    } else {
+        $intBet_vote = (int) $bet_vote;
+        if ($intBet_vote >= 100 && $intBet_vote <= 1000) {
+            $outadd = update_post_meta($bet_id , 'bet_vote', $bet_vote);
+            if ($outadd === true) {
+                $backArray['success'] = true;
+                $backArray['html'] = '<div class="alert alert-success" role="alert">Запись в post meta bet_vote обновлена!</div>';
+            } elseif($outadd === false) {
+                $backArray['success'] = false;
+                $backArray['html'] = '<div class="alert alert-danger" role="alert">Запись не добавлена или передано такое же значение поля!</div>';
+            } else {
+                $backArray['success'] = true;
+                $backArray['html'] = '<div class="alert alert-success" role="alert">Запись в post meta bet_vote - '.$outadd.' добавлена!</div>';
+            }
+        } else {
+            $backArray['success'] = false;
+            $backArray['html'] = '<div class="alert alert-danger" role="alert">Введите значения от 100 до 1000!</div>';
+        }  
+    }
+    
+    echo json_encode($backArray);
+    
+    wp_die();
+}
+
+add_action('wp_footer', 'best_action_javascript', 99); // для фронта
+function best_action_javascript() {
+    ?>
+    <script type="text/javascript" >
+    jQuery(document).ready(function($) {
+        var data = {
+                action: 'best_action',
+                betvote: 0,
+                betid: 0
+        };
+        var bet_buttom = document.getElementById('add_bet_vote');
+        if (bet_buttom != null) {
+            bet_buttom.onclick = function() {
+                var bet_container = document.querySelector("#bet-container");
+                var bet_input = bet_container.querySelector(".value_vote");
+                var bet_vote = bet_input.value;
+                var bet_id = this.getAttribute('bet_id');
+
+                if (bet_vote ===  "" || isNaN(bet_vote)) {
+                    alert('Введите значения от 100 до 1000');
+                    bet_input.focus();
+                    return;
+                }
+                let intBet_vote = Number(bet_vote);
+                if (!(intBet_vote >= 100 && intBet_vote <= 1000)) {
+                    alert('Введите значения от 100 до 1000');
+                    bet_input.focus();
+                    return;
+                }
+                data.betvote = bet_vote;
+                data.betid = bet_id;
+                //ajax
+                //console.log(data);
+                jQuery.post( btajax.url, data, function(response) {
+                    //удаляем все alerts
+                    console.log(response);
+                    let alertDiv = document.querySelector('.alert');
+                    if (alertDiv !== null) alertDiv.remove();
+                    if (response.success) {
+                        //Делаем кнопку недоступной
+                        bet_buttom.setAttribute('disabled',false);
+                    }
+                    bet_container.insertAdjacentHTML('afterbegin', response.html);
+
+                },'json');
+            };
+        }
+        
+        
+    });
+    </script>
+    <?php
+}
